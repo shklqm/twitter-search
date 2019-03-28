ef69a8e7986a6d13a8dd2bb56fa098116dde9eab
blediboss <bossbledi@gmail.com>
2019-03-28 22:34:27 +0300

    udpated urls.py file with the new urls

--- src/web/urls.py
+++ src/web/urls.py
@@ -1,7 +1,7 @@
 """web URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.0/topics/http/urls/
+    https://docs.djangoproject.com/en/2.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -13,9 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path
 
+from twitter.views import SearchByHashTag, GetUserTweets
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('hashtags/<keyword>', SearchByHashTag.as_view()),
+    path('users/<username>', GetUserTweets.as_view()),
 ]
